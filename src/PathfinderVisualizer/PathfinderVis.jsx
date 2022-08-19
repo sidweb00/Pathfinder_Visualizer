17e41e9f1b861831dd5ececc928801610f286e15
sidweb00 <siddharthgandhi005@gmail.com>
2022-08-19 18:25:59 +0530

    grid

--- src/PathfinderVisualizer/PathfinderVis.jsx
+++ src/PathfinderVisualizer/PathfinderVis.jsx
@@ -1,18 +1,44 @@
 import React, { Component } from 'react'
+import Node from './Node/Node';
+
+import './PathfinderVis.css'
 
 export default class PathfinderVis extends Component {
   
     constructor(props){
-        super(props);
-        this.state={};
+      super(props);
+      this.state={
+        nodes: [],
+      };
     }
 
+    componentDidMount(){
+      const nodes=[];
+      for(let row=0;row<=15;row++){
+        const currRow=[];
+        for(let col=0;col<=50;col++){
+          currRow.push([]);
+        }
+        nodes.push(currRow);
+      }
+      this.setState({nodes});
+    }
+
+
+
     render() {
+      const {nodes}=this.state
+      console.log(nodes)
     return (
-      <div>
-        Temporary
+      <div className='grid'>
+          {nodes.map((row,rindex)=>{
+            return <div>
+              {row.map((node,nodeind)=><Node></Node>)}
+            </div>
+          })}
       </div>
     )
   }
 }
+
 
